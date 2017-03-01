67b0a5f54282374328816b8ad21de4d8e668a78d
hg <hg@hyperspec1.higp.soest.hawaii.edu>
2017-03-01 12:22:06 -1000

    added myimwidget

---
+++ myimwidget.cpp
@@ -0,0 +1,6 @@
+#include "myimwidget.h"
+
+MyIMWidget::MyIMWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ myimwidget.h
@@ -0,0 +1,17 @@
+#ifndef MYIMWIDGET_H
+#define MYIMWIDGET_H
+
+#include <QWidget>
+
+class MyIMWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MyIMWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // MYIMWIDGET_H
